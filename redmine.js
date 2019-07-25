35fd318c2c8a6a09ad6432361858850b1c392cb8
zestagio <benya.drupal@gmail.com>
2019-07-25 22:43:21 +0300

    First vesrion of the implementation

---
+++ redmine.js
@@ -0,0 +1,40 @@
+(function ($) {
+  'use strict';
+
+  $(document).ready(function() {
+    var $title = $('div.issue div.subject h3');
+    if ($title.length) {
+      gitBranchAndCommit($title);
+    }
+
+    function gitBranchAndCommit($el) {
+      var issueKey = $el.parents('div.issue').prev().text();
+      var issueTitle = $el.text();
+      var issueBranch = issueKey.match(/\d+/) + '-' + issueTitle.trim()
+        .replace(/\[.*?\][\s\"\']*/g, '')
+        .replace(/\W/g, '-')
+        .replace(/-{2,10}/, '-')
+        .replace(/[-]+$/, '')
+        .toLowerCase();
+      var issueCommit = issueKey + ': ' + issueTitle;
+
+      var content = '';
+      content += '<strong>Branch: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueBranch + '</span></strong><br />';
+      content += '<strong>Commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueCommit + '</span></strong><br />';
+      content += '<strong>Short commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueKey + ': </span></strong>';
+
+      $el.after(content);
+
+      $('span.copy-me').off('click').on('click', function () {
+        var $this = $(this);
+        navigator.clipboard.writeText($this.text()).then(function () {
+          $this.fadeOut('slow', function () {
+            $this.fadeIn('slow');
+          });
+
+          $.notify($this.text() + ' copied to clipboard', 'success');
+        });
+      });
+    }
+  });
+})(window.jQuery);
---
+++ tampermonkey.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Redmine Helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Yauheni Harbuzau
+// @include      https://prj.*.com/issues/*
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
+// @require      https://raw.githubusercontent.com/zestagio/redmine-helper/master/redmine.js
+// ==/UserScript==
