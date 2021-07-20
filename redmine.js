fc204c587953ec6e0df7f11ef31e3e0a6a9fa0e3
Yauheni Harbuzau <yauheni.harbuzau@smile.fr>
2021-07-20 23:17:49 +0300

    Update redmine.js

--- redmine.js
+++ redmine.js
@@ -1,40 +1,40 @@
 (function ($) {
-  'use strict';
+    'use strict';
 
-  $(document).ready(function() {
-    var $title = $('div.issue div.subject h3');
-    if ($title.length) {
-      gitBranchAndCommit($title);
-    }
+    $(document).ready(function() {
+        let $title = $('div.issue div.subject h3');
+        if ($title.length) {
+            gitBranchAndCommit($title);
+        }
 
-    function gitBranchAndCommit($el) {
-      var issueKey = $el.parents('div.issue').prev().text();
-      var issueTitle = $el.text();
-      var issueBranch = issueKey.match(/\d+/) + '-' + issueTitle.trim()
-        .replace(/\[.*?\][\s\"\']*/g, '')
-        .replace(/\W/g, '-')
-        .replace(/-{2,}/g, '-')
-        .replace(/[-]+$/, '')
-        .toLowerCase();
-      var issueCommit = issueKey + ': ' + issueTitle;
+        function gitBranchAndCommit($el) {
+            let issueKey = $el.parents('div.issue').prev().text();
+            let issueTitle = $el.text();
+            let issueBranch = issueKey.match(/\d+/) + '-' + issueTitle.trim()
+                .replace(/\[.*?][\s"']*/g, '')
+                .replace(/\W/g, '-')
+                .replace(/-{2,}/g, '-')
+                .replace(/[-]+$/, '')
+                .toLowerCase();
+            let issueCommit = issueKey + ': ' + issueTitle;
 
-      var content = '';
-      content += '<strong>Branch: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueBranch + '</span></strong><br />';
-      content += '<strong>Commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueCommit + '</span></strong><br />';
-      content += '<strong>Short commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueKey + ': </span></strong>';
+            let content = '';
+            content += '<strong>Branch: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueBranch + '</span></strong><br />';
+            content += '<strong>Commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueCommit + '</span></strong><br />';
+            content += '<strong>Short commit: <span class="copy-me" style="color:#d22;cursor:pointer;">' + issueKey + ': </span></strong>';
 
-      $el.after(content);
+            $el.after(content);
 
-      $('span.copy-me').off('click').on('click', function () {
-        var $this = $(this);
-        navigator.clipboard.writeText($this.text()).then(function () {
-          $this.fadeOut('slow', function () {
-            $this.fadeIn('slow');
-          });
+            $('span.copy-me').off('click').on('click', function () {
+                let $this = $(this);
+                navigator.clipboard.writeText($this.text()).then(function () {
+                    $this.fadeOut('slow', function () {
+                        $this.fadeIn('slow');
+                    });
 
-          $.notify($this.text() + ' copied to clipboard', 'success');
-        });
-      });
-    }
-  });
+                    $.notify($this.text() + ' copied to clipboard', 'success');
+                });
+            });
+        }
+    });
 })(window.jQuery);
